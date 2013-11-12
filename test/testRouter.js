7dadd7c4e9c1aad005fa8cb943b7a8c3a903965b
jay-depot <unleet.42@gmail.com>
2013-11-12 11:20:40 -0500

    Change the expected router behavior to something a little more sensible
    The router will now return false as the controller in the case of a 404. This should make writing the 404 handling logic in derived apps easier.

--- test/testRouter.js
+++ test/testRouter.js
@@ -17,7 +17,7 @@ exports['test root route resolves properly'] = function(assert) {
 exports['test url not in table resolves to 404'] = function(assert) {
   Router.routes({});
   var route = Router.resolve('/obviously-does-not-exist');
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
 };
 
 exports['test static routes resolve properly'] = function(assert) {
@@ -25,22 +25,22 @@ exports['test static routes resolve properly'] = function(assert) {
   var route = Router.resolve("/test");
   assert.equal(route.controller, "Test", "Expected: 'Test'");
   route = Router.resolve('/test/passes');
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
   route = Router.resolve("/example/subpath");
   assert.equal(route.controller, "Example", "Expected: 'Example'");
   route = Router.resolve('/example/this-is-not-one');
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
 };
 
 exports['test routes with one variable resolve properly'] = function(assert) {
   Router.routes({"test/:var1" : "Test"});
   var route = Router.resolve("/test");
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
   route = Router.resolve("/test/passes/with-flying-colors");
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
   route = Router.resolve("/test/passes");
   assert.equal(route.controller, "Test", "Expected: 'Test'");
   assert.equal(route.var1, "passes", "Expected: var1 == 'passes'");
   route = Router.resolve("/test/passes/with-flying-colors");
-  assert.equal(route.controller, "Error404", "Expected: 'Error404'");
+  assert.equal(route.controller, false, "Expected: false");
 };
