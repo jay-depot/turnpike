be95d58c8822bc28ecb625baf6eebf472b0080fc
Jason <jason.depeaux@gmail.com>
2013-11-15 17:37:16 -0500

    Add empty tests for every component I know will exist so far.

---
+++ test/testEndpointController.js
@@ -0,0 +1,9 @@
+/**
+ * Tests the EndpointController class.
+ */
+
+var EndpointController = require('../lib/EndpointController');
+
+exports['test EndpointRouter.extend gives inheritor expected properties'] = function(assert){
+
+};
