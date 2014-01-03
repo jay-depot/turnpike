abdfdc644fce009fb5b7d4a59bc9f16e7fead9e3
jay-depot <unleet.42@gmail.com>
2014-01-02 19:54:26 -0500

    Make test descriptions more useful.
    Note, for validity of tests, they still fail.

--- test/testRouter.js
+++ test/testRouter.js
@@ -11,36 +11,34 @@ exports['test create new router from object'] = function(assert) {
 exports['test root route resolves properly'] = function(assert) {
   Router.routes({});
   var route = Router.resolve('/');
-  assert.equal(route.controller, "Index", "Expected: 'Index'");
+  assert.equal(route.controller, "Index", "'/' should resolve to 'Index'");
 };
 
 exports['test url not in table resolves to 404'] = function(assert) {
   Router.routes({});
   var route = Router.resolve('/obviously-does-not-exist');
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, false, "Resolving a nonexistent route gives back false");
 };
 
 exports['test static routes resolve properly'] = function(assert) {
   Router.routes({"/test" : "Test", "/example/subpath" : "Example"});
   var route = Router.resolve("/test");
-  assert.equal(route.controller, "Test", "Expected: 'Test'");
+  assert.equal(route.controller, "Test", "Static single-part route returns correct controller name");
   route = Router.resolve('/test/passes');
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, false, "Subpath of existing static route returns false");
   route = Router.resolve("/example/subpath");
-  assert.equal(route.controller, "Example", "Expected: 'Example'");
+  assert.equal(route.controller, "Example", "Static two-part route returns correct controller name");
   route = Router.resolve('/example/this-is-not-one');
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, false, "Partial match on static two-part route returns false");
 };
 
 exports['test routes with one variable resolve properly'] = function(assert) {
   Router.routes({"/test/:var1" : "Test"});
   var route = Router.resolve("/test");
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, false, "Route with variable does not match path without one");
   route = Router.resolve("/test/passes/with-flying-colors");
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, false, "Route with one variable does not match path with extra variables");
   route = Router.resolve("/test/passes");
-  assert.equal(route.controller, "Test", "Expected: 'Test'");
-  assert.equal(route.params.var1, "passes", "Expected: var1 == 'passes'");
-  route = Router.resolve("/test/passes/with-flying-colors");
-  assert.equal(route.controller, false, "Expected: false");
+  assert.equal(route.controller, "Test", "Controller name is returned correctly from route with variable");
+  assert.equal(route.params.var1, "passes", "Variable given in path is returned correctly in route");
 };
