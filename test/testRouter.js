36bebed1429666f516e135beac70ae9511ff57ae
jay-depot <unleet.42@gmail.com>
2014-01-12 22:29:48 -0500

    Change Router tests for Async Router

--- test/testRouter.js
+++ test/testRouter.js
@@ -5,40 +5,76 @@
 var Router = require('../lib/Router');
 
 exports['test create new router from object'] = function(assert) {
-  Router.routes({"/test":"Test"});
+  Router.routes({'/test':'Test'});
 };
 
-exports['test root route resolves properly'] = function(assert) {
+exports['test root route resolves properly'] = function(assert, done) {
   Router.routes({});
-  var route = Router.resolve('/');
-  assert.equal(route.controller, "Index", "'/' should resolve to 'Index'");
+  Router.resolve(function(err, route) {
+    assert.equal(route.controller, 'Index', '"/" should resolve to "Index"');
+    done();
+  }, '/');
 };
 
-exports['test url not in table resolves to 404'] = function(assert) {
+exports['test url not in table resolves to 404'] = function(assert, done) {
   Router.routes({});
-  var route = Router.resolve('/obviously-does-not-exist');
-  assert.equal(route.controller, false, "Resolving a nonexistent route gives back false");
+  Router.resolve(function(err, route) {
+    assert.equal(route.controller, false, "Resolving a nonexistent route gives back false");
+    done();
+  }, '/obviously-does-not-exist');
 };
 
-exports['test static routes resolve properly'] = function(assert) {
-  Router.routes({"/test" : "Test", "/example/subpath" : "Example"});
-  var route = Router.resolve("/test");
-  assert.equal(route.controller, "Test", "Static single-part route returns correct controller name");
-  route = Router.resolve('/test/passes');
-  assert.equal(route.controller, false, "Subpath of existing static route returns false");
-  route = Router.resolve("/example/subpath");
-  assert.equal(route.controller, "Example", "Static two-part route returns correct controller name");
-  route = Router.resolve('/example/this-is-not-one');
-  assert.equal(route.controller, false, "Partial match on static two-part route returns false");
+exports['test static routes resolve properly'] = function(assert, done) {
+  Router.routes({'/test' : 'Test', '/example/subpath' : 'Example'});
+  Router.resolve(function(err, route) {
+    assert.equal(route.controller, "Test", "Static single-part route returns correct controller name");
+    Router.resolve(function(err, route) {
+      assert.equal(route.controller, false, "Subpath of existing static route returns false");
+      Router.resolve(function(err, route) {
+        assert.equal(route.controller, "Example", "Static two-part route returns correct controller name");
+        Router.resolve(function(err, route) {
+          assert.equal(route.controller, false, "Partial match on static two-part route returns false");
+          done();
+        }, '/example/this-is-not-one');
+      }, '/example/subpath');
+    }, '/test/passes');
+  }, '/test');
 };
 
-exports['test routes with one variable resolve properly'] = function(assert) {
+exports['test routes with one variable resolve properly'] = function(assert, done) {
   Router.routes({"/test/:var1" : "Test"});
-  var route = Router.resolve("/test");
-  assert.equal(route.controller, false, "Route with variable does not match path without one");
-  route = Router.resolve("/test/passes/with-flying-colors");
-  assert.equal(route.controller, false, "Route with one variable does not match path with extra variables");
-  route = Router.resolve("/test/passes");
-  assert.equal(route.controller, "Test", "Controller name is returned correctly from route with variable");
-  assert.equal(route.params.var1, "passes", "Variable given in path is returned correctly in route");
+  Router.resolve(function(err, route){
+    assert.equal(route.controller, false, "Route with variable does not match path without one");
+    Router.resolve(function(err, route){
+      assert.equal(route.controller, false, "Route with one variable does not match path with extra variables");
+      Router.resolve(function(err, route){
+        assert.equal(route.controller, "Test", "Controller name is returned correctly from route with variable");
+        assert.equal(route.params.var1, "passes", "Variable given in path is returned correctly in route");
+        done();
+      }, '/test/passes');
+    }, '/test/passes/with-flying-colors');
+  }, '/test');
+};
+
+exports['test pre/post hooks fire'] = function(assert, done) {
+  var pre   = false
+    , post  = false
+    , route;
+
+  Router.routes({});
+  Router.pre('resolve', function(next, path) {
+    pre = true;
+    next(path);
+  });
+  Router.post('resolve', function(next, path) {
+    post = true;
+    next(path);
+  });
+  Router.resolve(function(err, route){
+    assert.ok(pre,  "pre-hook fires");
+    process.nextTick( function() {
+      assert.ok(post, "post-hook fires");
+      done();
+    });
+  }, '/');
 };
