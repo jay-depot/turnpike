eb67e74877f47d65700993e185d0ed051d90d78a
jay-depot <unleet.42@gmail.com>
2014-04-06 18:52:28 -0400

    Add default access control rules to framework and default project

---
+++ bin/skeletons/project/api/access/Index.js
@@ -0,0 +1,11 @@
+/**
+ * Access control rules for the Index controller.
+ * These rules will control access to your home page. Typically you want to
+ * keep this permissive.
+ */
+
+function Index() {
+  return true;
+}
+
+module.exports = Index;
---
+++ bin/skeletons/project/api/access/Page.js
@@ -0,0 +1,11 @@
+/**
+ * Access control rules for the Page controller.
+ * Typically you want this to allow all access, and handle more restrictive
+ * cases inside specific actions.
+ */
+
+function Page() {
+  return true;
+}
+
+module.exports = Page;
