4b29ccfcc9a10f4861e06e635a09941a0a306f17
unleet <unleet.42@gmail.com>
2014-09-06 17:22:49 -0400

    Add placeholders for integration tests

---
+++ test/integration/testSkeleton.js
@@ -0,0 +1,5 @@
+/**
+ * Integration test.
+ *
+ * Create a dummy project in /tmp, link in turnpike, and test connections to it.
+ */
---
+++ test/testIntegration.js
@@ -0,0 +1,4 @@
+/**
+ * Run each test under ./integration, one at a time.
+ * Each one will likely need to pollute global namespace, and thus will need to be run in a forked process.
+ */
