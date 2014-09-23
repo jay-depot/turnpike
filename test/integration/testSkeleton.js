667af8803e4bd16c8b6da41e4a3828d8c5d171a0
unleet <unleet.42@gmail.com>
2014-09-22 20:34:16 -0400

    Beginning of the "Skeleton" test. Creates a default project.

--- test/integration/testSkeleton.js
+++ test/integration/testSkeleton.js
@@ -3,3 +3,10 @@
  *
  * Create a dummy project in /tmp, link in turnpike, and test connections to it.
  */
+
+var shell = require('shelljs');
+
+console.log('Creating a project skeleton in', shell.tempdir());
+shell.cd(shell.tempdir());
+shell.exec('turnpike create project Turnpike Test');
+
