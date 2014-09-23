94c381cf8755a61b49996f7572115eb41455efd5
unleet <unleet.42@gmail.com>
2014-09-22 20:33:30 -0400

    Runner to find and execute each integration test.

--- test/testIntegration.js
+++ test/testIntegration.js
@@ -2,3 +2,24 @@
  * Run each test under ./integration, one at a time.
  * Each one will likely need to pollute global namespace, and thus will need to be run in a forked process.
  */
+
+var fs = require('fs');
+var files = fs.readdirSync(fs.realpathSync('./test/integration'));
+var shell = require('shelljs');
+var pattern = /^(test)\w*\.js$/;
+
+for (var i in files) {
+  if (files.hasOwnProperty(i)) {
+    pattern.lastIndex = 0;
+    if (pattern.test(files[i])) {
+      exports[files[i]] = function(file, assert) {
+        file = './test/integration/' + file;
+        console.log(file);
+        shell.exec('node ' + file, function(code, output) {
+          console.log(output);
+          assert.equal(code, 0, "Integration run must exit with 0 status");
+        });
+      }.bind(this, files[i])
+    }
+  }
+}
