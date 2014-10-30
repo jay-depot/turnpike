7954ccadf488c01a490868bf0c43ee2491ce92f0
unleet <unleet.42@gmail.com>
2014-10-29 20:20:52 -0400

    Convert page delivery to use Renderable everywhere.

--- bin/skeletons/project/application/controllers/Page.js
+++ bin/skeletons/project/application/controllers/Page.js
@@ -5,32 +5,27 @@ var turnpike = require('turnpike');
 var util     = require('util');
 var _        = turnpike.imports.underscore;
 
+/**
+ * Controller that composes page elements and prepares a complete HTML document for delivery.
+ *
+ * @param connection
+ * @constructor
+ */
 function Page(connection) {
-  var $this = this;
-  turnpike.classes.base.controller.EndpointController.call(this, connection);
+  turnpike.classes.base.controller.PageComposerController.call(this, connection);
+  this.view = turnpike.application.views.Page;
 
-  //TODO: Should we send error if we get here and the content type isn't HTML?
-  this.deliver = function() {
-    var view = new turnpike.application.views.Page();
-    view.mode('main').data($this.data)
-      .render(function(html) {
-        connection.response(html).send();
-      });
+  this.prepare = function(next) {
+    this.mode = 'main';
+
+    this.data = {
+      'body':  this.connection.response(),
+      'title': this.connection.controller.title + ' | ' + turnpike.config.sitename
+    };
+
+    process.nextTick(next);
   };
 }
-util.inherits(Page, turnpike.classes.base.controller.EndpointController);
-
-
-Page.prototype._GET =
-Page.prototype._PUT =
-Page.prototype._POST =
-Page.prototype._DELETE = function(readyCallback) {
-  this.data = {
-    'body':  this.connection.response(),
-    'title': this.connection.controller.title + ' | ' + turnpike.config.sitename
-  };
-
-  process.nextTick(readyCallback);
-};
+util.inherits(Page, turnpike.classes.base.controller.PageComposerController);
 
 module.exports = Page;
