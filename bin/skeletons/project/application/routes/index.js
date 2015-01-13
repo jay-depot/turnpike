29acab5eb382c43cee450c96ec845d4046690d09
unleet <unleet.42@gmail.com>
2015-01-12 20:01:42 -0500

    Remove old router. Add new routing system based on Express.
    File upload handling middleware is removed from the default stack, and will be rewritten so it can be added
    to individual routes through the new route declaration API instead

---
+++ bin/skeletons/project/application/routes/index.js
@@ -0,0 +1,10 @@
+/**
+ * Routing for Index controller.
+ */
+
+var turnpike = require('turnpike');
+
+module.exports = function() {
+  turnpike.router.path('/').
+    get(turnpike.action('Index'));
+};
